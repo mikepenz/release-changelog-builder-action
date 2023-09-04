512b4d3a42bae2567c5ba26a40da3031d939fbe7
Mike Penz <mikepenz@gmail.com>
2023-09-04 13:52:54 +0000

    - upgrade to node20 for the GitHub actions - remove node-fetch as it is no longer required with node 20 - upgrade actions to use node20

--- pr-collector/src/prCollector.ts
+++ pr-collector/src/prCollector.ts
@@ -6,7 +6,6 @@ import {failOrError} from './utils'
 import {HttpsProxyAgent} from 'https-proxy-agent'
 import {PullRequestInfo, PullRequests} from './pullRequests'
 import {Commits, DiffInfo} from './commits'
-import fetch from 'node-fetch'
 
 export interface Options {
   owner: string // the owner of the repository
@@ -62,10 +61,7 @@ export class PullRequestCollector {
     // load octokit instance
     const octokit = new Octokit({
       auth: `token ${this.token || process.env.GITHUB_TOKEN}`,
-      baseUrl: `${this.baseUrl || 'https://api.github.com'}`,
-      request: {
-        fetch
-      }
+      baseUrl: `${this.baseUrl || 'https://api.github.com'}`
     })
 
     if (proxy) {
