b7c5179f2c6841e692e96f8c365c1483ae2e7629
Mike Penz <opensource@mikepenz.dev>
2024-03-01 20:15:08 +0000

    - add one more case to the nested testcase

--- __tests__/transformNested.test.ts
+++ __tests__/transformNested.test.ts
@@ -19,7 +19,8 @@ pullRequestsWithLabels.push(
   buildPullRequeset(4, 'Mobile Bug Ticket', ['mobile', 'bug']),
   buildPullRequeset(5, 'Mobile & Core Feature Ticket', ['core', 'mobile', 'feature']),
   buildPullRequeset(6, 'Mobile & Core Bug Ticket', ['core', 'mobile', 'bug']),
-  buildPullRequeset(7, 'Mobile & Core Bug Bug Ticket', ['core', 'mobile', 'bug', 'fancy-bug'])
+  buildPullRequeset(7, 'Mobile & Core Bug Bug Ticket', ['core', 'mobile', 'bug', 'fancy-bug']),
+  buildPullRequeset(8, 'Core Ticket', ['core'])
 )
 
 it('Match multiple labels exhaustive for category', async () => {
@@ -79,5 +80,6 @@ it('Match multiple labels exhaustive for category', async () => {
     }
   ]
 
-  expect(buildChangelogTest(customConfig, pullRequestsWithLabels, repositoryUtils)).toStrictEqual(``)
+  const built = buildChangelogTest(customConfig, pullRequestsWithLabels, repositoryUtils)
+  expect(built).toStrictEqual(`## Core\n\n- Core Ticket\n\n### 🚀 Features\n\n- Core Feature Ticket\n- Mobile & Core Feature Ticket\n\n### 🧪 Bug\n\n- Core Bug Ticket\n- Mobile & Core Bug Ticket\n\n#### 🧪 Bug Bug\n\n- Mobile & Core Bug Bug Ticket\n\n## Mobile\n\n\n### 🚀 Features\n\n- Mobile Feature Ticket\n\n### 🧪 Bug\n\n- Mobile Bug Ticket\n\n`)
 })
