b8f52e6547f300be3dc4443c6e851aa0176c55e3
Bob Loblaw <iwrite@selfdocumentingco.de>
2024-09-14 20:37:36 +0200

    Revert assertion in configuration.tests

--- __tests__/configuration.test.ts
+++ __tests__/configuration.test.ts
@@ -15,31 +15,9 @@ it('Configurations are merged correctly', async () => {
 
   const mergedConfiguration = mergeConfiguration(configurationJson, configurationFile)
 
-  const expectedConfiguration: Configuration = {
-    max_tags_to_fetch: 200,
-    max_pull_requests: 1000,
-    max_back_track_time_days: 1000,
-    exclude_merge_branches: [],
-    sort: 'DESC',
-    template: '#{{CHANGELOG}}',
-    pr_template: '- #{{TITLE}}\n   - PR: ##{{NUMBER}}',
-    commit_template: '- #{{TITLE}}',
-    empty_template: '- no magic changes',
-    categories: [
-      {title: '## 🚀 Features', labels: ['feature']},
-      {title: '## 🐛 Fixes', labels: ['fix']},
-      {title: '## 🧪 Tests', labels: ['test']}
-    ],
-    ignore_labels: ['ignore'],
-    label_extractor: [],
-    transformers: [],
-    tag_resolver: {method: 'semver'},
-    base_branches: [],
-    custom_placeholders: [],
-    trim_values: true,
-    categorized_include_empty_content: false
-  }
+  const expectedStringified = JSON.stringify(mergedConfiguration)
 
-  console.log(mergedConfiguration)
-  expect(JSON.stringify(mergedConfiguration)).toEqual(JSON.stringify(expectedConfiguration))
+  expect(expectedStringified).toEqual(
+    `{"max_tags_to_fetch":200,"max_pull_requests":1000,"max_back_track_time_days":1000,"exclude_merge_branches":[],"sort":"DESC","template":"#{{CHANGELOG}}","pr_template":"- #{{TITLE}}\\n   - PR: ##{{NUMBER}}","commit_template":"- #{{TITLE}}","empty_template":"- no magic changes","categories":[{"title":"## 🚀 Features","labels":["feature"]},{"title":"## 🐛 Fixes","labels":["fix"]},{"title":"## 🧪 Tests","labels":["test"]}],"ignore_labels":["ignore"],"label_extractor":[],"transformers":[],"tag_resolver":{"method":"semver"},"base_branches":[],"custom_placeholders":[],"trim_values":true,"categorized_include_empty_content":false}`
+  )
 })
